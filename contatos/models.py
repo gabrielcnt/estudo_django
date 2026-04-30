2c83a6d88ece562fb89d53a009abd54c3126f907
Gabriel Vieira <gabrielcnt96@gmail.com>
2026-04-29 22:40:52 -0300

    feat: criação da model e migrations

---
+++ contatos/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Contato(models.Model):
+    nome = models.CharField(max_length=100)
+    email = models.EmailField()
+    telefone = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.nome
